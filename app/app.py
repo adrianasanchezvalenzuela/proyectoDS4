ed9c16ee53713b8845c87dc6d55038010f4fed10
adrianasanchezvalenzuela <alysan1505@gmail.com>
2025-05-09 18:52:09 -0700

    plantilla login hecha

--- app/app.py
+++ app/app.py
@@ -36,5 +36,9 @@ def busqueda():
 def creditos():
     return render_template("creditos.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
