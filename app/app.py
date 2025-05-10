7dc7ec5798428a4f1c93303cc19e88d1e788082d
adrianasanchezvalenzuela <alysan1505@gmail.com>
2025-05-09 17:07:24 -0700

    app.py solo dirige a paginas

--- app/app.py
+++ app/app.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/inicio")
+def index_redirect():
+    return render_template("index.html")
+
+@app.route("/area")
+def area():
+    return render_template("area.html")
+
+@app.route("/explorar")
+def explorar():
+    return render_template("explorar.html")
+
+@app.route("/catalogos")
+def catalogos():
+    return render_template("catalogos.html")
+
+@app.route("/buscar")
+def buscar():
+    query = request.args.get('q', '')
+    return render_template("busqueda.html", query=query)
+
+@app.route("/creditos")
+def creditos():
+    return render_template("creditos.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
