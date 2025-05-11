c688da437cb32c27c3047bec766dcbaa2c77e190
marcoaquintanar <marcoaquintanar@outlook.com>
2025-05-11 02:01:41 -0700

    Se unieron los json con las revistas buscadas, parte 2 completamente terminada

--- gestion_json/unir_resultados.py
+++ gestion_json/unir_resultados.py
@@ -1,6 +1,6 @@
 import json
 
-archivos = ['datos/json/salida_B.json', 'datos/json/salida_C.json', 'datos/json/salida_D.json']
+archivos = ['datos/json/salida_A.json', 'datos/json/salida_A2.json', 'datos/json/revistas_info_parte_1.json', 'datos/json/salida_E.json']
 resultado = {}
 
 for archivo in archivos:
@@ -8,7 +8,7 @@ for archivo in archivos:
         datos = json.load(f)
         resultado.update(datos)
 
-with open('datos/json/revistas_info_parte_1.json', 'w', encoding='utf-8') as f:
+with open('datos/json/revistas_info_scimago.json', 'w', encoding='utf-8') as f:
     json.dump(resultado, f, indent=2, ensure_ascii=False)
 
 print("Resultados guardados correctamente :)")
